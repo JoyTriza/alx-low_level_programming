ddf3e140eb5b36bcb77016dd087d950a182835f3
JoyTriza <wangarijoytriza@gmail.com>
2023-03-17 15:09:47 -0700

    Add alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char c;
+	char low;
 
-	char d;
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
 
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'Z') {
-			putchar(d);
-			d++
-		}
+	for(low = 'A'; low <= 'Z'; low++)
+	putchar(low);
 	putchar('\n');
+
 	return (0);
 }
