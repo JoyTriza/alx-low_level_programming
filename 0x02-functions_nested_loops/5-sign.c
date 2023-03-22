4dbc93379f860d0e929db30a059bbab2f347374a
JoyTriza <wangarijoytriza@gmail.com>
2023-03-22 00:18:57 -0700

    Add signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign -  prints the sign of a number.
  * @n: The number to be checked
- * Return: 1 for positive numbers, -1 for negative numbers or 0 for anything else
+ * Return: 1 for positive num, -1 for negative num or 0 for anything else
  */
 
 int print_sign(int n)
