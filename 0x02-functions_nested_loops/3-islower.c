f65efb7273ffb54fbb95ca0608eb4a7c065c368d
JoyTriza <wangarijoytriza@gmail.com>
2023-03-21 23:57:56 -0700

    Add is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: Thexter to be checked
+ * Return: 1 for lowercase xters or 0 anyhting for anyhting else.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
+
