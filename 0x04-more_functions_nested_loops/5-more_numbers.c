025bc880423a0aaeb56d70d8855f807d80a02549
JoyTriza <wangarijoytriza@gmail.com>
2023-03-24 11:28:33 -0700

    Add universal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,10 @@ void more_numbers(void)
 
 	for (x = 1; x <= 10; x++)
 	{
-	for (y = 0; y <= 14; y++)
+	for (y = 0; y <= 15; y++)
 	{
 	if (y >= 10)
-	_putchar('1);
+	_putchar('1');
 
 	_putchar(j % 10 + '0');
 	}
