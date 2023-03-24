f954f72dfd64618edd183ed5b29f2e94217a00b1
JoyTriza <wangarijoytriza@gmail.com>
2023-03-24 13:06:40 -0700

    Add lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,15 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 1; x <= 10; x++)
+	for (x = 0; x < 10; x++)
 	{
-	for (y = 0; y <= 15; y++)
+	for (y = 0; y <= 14; y++)
 	{
-	if (y >= 10)
-	_putchar('1');
-
-	_putchar(j % 10 + '0');
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
 	}
