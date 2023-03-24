da29e96e4ea3a5768e3422e621d57d455932fdc6
JoyTriza <wangarijoytriza@gmail.com>
2023-03-24 06:04:33 -0700

    Add upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,13 @@
  * @x: the number being checked
  * Return: 1 if c is uppercase and 0 if not
  */
-
-int _isupper(int c);
+int _isupper(int x);
 {
+
 	if (x >= 65 && x <= 90)
 	{
 	return (1);
 	}
 	return (0);
+
 }	
