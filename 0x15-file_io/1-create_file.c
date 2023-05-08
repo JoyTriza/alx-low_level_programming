5a9e11abce0ab4b9de3f7a8062ab1671cd64b354
JoyTriza <wangarijoytriza@gmail.com>
2023-05-08 13:15:13 -0700

    Add 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,13 @@ int create_file(const char *filename, char *text_content)
 
 {
 	int o, w, len = o;
+
 	if (filename == NULL)
 		return (-1);
 
-	if (text_context != NULL)
+	if (text_content != NULL)
 	{
-		for (len = 0; text_context[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
